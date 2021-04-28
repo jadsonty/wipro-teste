a10f36b42d32e68b27e5ab36c3f459c4be9ed099
Jadson José <jadsonjose0123@gmail.com>
2021-04-28 13:55:30 -0300

    Finalizado especificações do projeto. Primeiro commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-teste';
+
+  headers = ["ID", "Idade", "Nome", "Posicao", "Clube"];
+
+  rows = [
+    {
+      "ID" : "1",
+      "Idade" : "18",
+      "Nome" : "Jorge",
+      "Posicao" : "Atacante",
+      "Clube" : "Barcelona"
+     },
+     {
+      "ID" : "2",
+      "Idade" : "23",
+      "Nome" : "Roberto",
+      "Posicao" : "Atacante",
+      "Clube" : "Real Madrid"
+     },
+     {
+      "ID" : "3",
+      "Idade" : "32",
+      "Nome" : "Junior",
+      "Posicao" : "Atacante",
+      "Clube" : "Valencia"
+     },
+     {
+      "ID" : "4",
+      "Idade" : "27",
+      "Nome" : "Rodriguinho",
+      "Posicao" : "Atacante",
+      "Clube" : "Athletic Bilbao"
+     },
+     {
+      "ID" : "5",
+      "Idade" : "32",
+      "Nome" : "Jair",
+      "Posicao" : "Atacante",
+      "Clube" : "Real Sociedad"
+     }
+]
 }
